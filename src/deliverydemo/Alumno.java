3f97d34506b62a064c99f9ac37806692fbf4dd2d
Jaime Farfan NET <jfarfannet@gmail.com>
2020-06-03 15:25:51 -0500

    clase Alumno

---
+++ src/deliverydemo/Alumno.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deliverydemo;
+
+/**
+ *
+ * @author jfarfan
+ */
+public class Alumno {
+    int codigo;
+    String nombre;
+    String email;
+}
